b7e29b8ee26c314422b3eacb77d8d6800fc33ea3
JanPfenning <jan.pfenning@gmail.com>
2021-02-22 14:59:46 +0100

    alter service url

--- src/app/sensor-api.service.ts
+++ src/app/sensor-api.service.ts
@@ -15,12 +15,12 @@ export class SensorApiService {
   }
 
   getTemp(table: string, from: number, to: number): Observable<string>{
-    return this.httpClient.get(`http://localhost:3000/sensor/${table}/${from}/${to}`,{responseType: 'text'});
+    return this.httpClient.get(`http://192.168.178.83:3000/sensor/${table}/${from}/${to}`,{responseType: 'text'});
     //return this.httpClient.get<any>(`localhost:3000/sensor/${from}/${to}`);
   }
 
   ping(): Observable<string>{
-    return this.httpClient.get(`http://localhost:3000/public/ping`,{responseType: 'text'});
+    return this.httpClient.get(`http://192.168.178.83:3000/public/ping`,{responseType: 'text'});
     //return this.httpClient.get<any>(`localhost:3000/sensor/${from}/${to}`);
   }
 }
