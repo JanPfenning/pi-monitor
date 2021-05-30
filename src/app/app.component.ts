e4798cd6cc9cb80ce44dfac840d3cde5a01b5d92
JanPfenning <jan.pfenning@gmail.com>
2021-05-30 10:34:54 +0200

    add excel export

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {SensorApiService} from "./sensor-api.service";
 export class AppComponent implements OnInit{
   title = 'Web-Interface';
   sensorService: SensorApiService;
-  tempPastDayJSON;
+  tempJSON;
   dataFromHoursAgo: number = 24;
   dataToHoursAgo: number = 0;
   public dataExists: boolean;
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit{
                     console.log('Observer got a next value: ' + x);
                     //this.tempPastDayJSON = x;
                     var json = JSON.parse(x);
-                    this.tempPastDayJSON = json;
+                    this.tempJSON = json;
                     this.dataExists = this.reasonableData()
                   },
       error: err => console.error('Observer got an error: ' + JSON.stringify(err)),
@@ -38,10 +38,59 @@ export class AppComponent implements OnInit{
   }
 
   reasonableData() {
-    return (Object.keys(this.tempPastDayJSON).length !== 0)
+    return (Object.keys(this.tempJSON).length !== 0)
   }
 
   reverseData() {
-    this.tempPastDayJSON = this.tempPastDayJSON.reverse()
+    this.tempJSON = this.tempJSON.reverse()
+  }
+
+  exportCsv() {
+    this.downloadFile(this.tempJSON);
+  }
+
+  downloadFile(data, filename = 'data') {
+    let arrHeader = ["name", "age", "country", "phone"];
+    let csvData = this.ConvertToCSV(data, arrHeader);
+    console.log(csvData)
+    let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
+    let dwldLink = document.createElement("a");
+    let url = URL.createObjectURL(blob);
+    let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+    if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
+      dwldLink.setAttribute("target", "_blank");
+    }
+    dwldLink.setAttribute("href", url);
+    dwldLink.setAttribute("download", "sample.csv");
+    dwldLink.style.visibility = "hidden";
+    document.body.appendChild(dwldLink);
+    dwldLink.click();
+    document.body.removeChild(dwldLink);
+  }
+
+  ConvertToCSV(objArray, headerList) {
+    console.log(objArray);
+    console.log(headerList);
+    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    let row = 'S.No,';
+
+    let newHeaders = ["Name", "Age", "Country", "Phone"];
+
+    for (let index in newHeaders) {
+      row += newHeaders[index] + ',';
+    }
+    row = row.slice(0, -1);
+    str += row + '\r\n';
+    for (let i = 0; i < array.length; i++) {
+      let line = (i + 1) + '';
+      for (let index in headerList) {
+        let head = headerList[index];
+
+        line += ',' + array[i][head];
+      }
+      str += line + '\r\n';
+    }
+    return str;
   }
 }
